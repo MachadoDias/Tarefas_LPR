217a0b58faa9beffa071e20bf307feffad42d749
MachadoDias <gabrielgabriel181007@gmail.com>
2025-06-06 19:14:33 -0300

    organizacao

--- EstruturasDeRepeticao/Exercicio3/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿int soma = 0;
-for(int i = 50; i<= 500; i++){
-    if(i%3 == 0 && i%2 != 0) soma += i;
-}
-Console.WriteLine(soma);
--- EstruturasDeSelecao/isEven/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿Console.Write("Digite o número: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(number%2 == 0 ? "PAR" : "IMPAR");
