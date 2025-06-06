9f7daa0aa71cfdb8adc51ac9fd2b361fe8fc39a9
MachadoDias <gabrielgabriel181007@gmail.com>
2025-06-06 19:13:07 -0300

    organizacao

--- VetoresEMatrizes/Exercicio3/obj/Debug/net9.0/Exercicio3.AssemblyInfo.cs
+++ VetoresEMatrizes/Exercicio3/obj/Debug/net9.0/Exercicio3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed18ae693ff1520ad3f5e66de1fa9cd218fa1717")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97f3aad91a84c4f8f840182901b54e8d987743a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
